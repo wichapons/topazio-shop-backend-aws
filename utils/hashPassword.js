7ded7ea16af5dd458e5aa1e8e65aae95e7a59008
Wichapon Viriyatharangkurn <wichapon@gmail.com>
2024-08-11 16:14:26 +0700

    chore: Update bcrypt dependency to bcryptjs for password hashing (complatible with docker)

--- utils/hashPassword.js
+++ utils/hashPassword.js
@@ -1,13 +1,13 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
-const salt = bcrypt.genSaltSync(10); //generate salt
+const salt = bcrypt.genSaltSync(10); // generate salt
 
 const hashPassword = (password) => {
   return bcrypt.hashSync(password, salt); // generate hash
 };
 
 const comparePasswords = (inputPassword, hashedPassword) => {
-  return bcrypt.compare(inputPassword, hashedPassword);
+  return bcrypt.compareSync(inputPassword, hashedPassword); // compare passwords
 };
 
-module.exports = { hashPassword,comparePasswords };
+module.exports = { hashPassword, comparePasswords };
